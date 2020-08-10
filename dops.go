9d306349ff0e54b2c3fe990d59bf778f38c3daae
MarvinJWendt <git@marvinjwendt.com>
2020-08-11 01:37:51 +0200

    Add more template code

--- dops.go
+++ dops.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"github.com/dops-cli/dops/modules"
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
+	"sort"
+)
+
+var (
+	CliFlags    []cli.Flag
+	CliCommands []*cli.Command
+)
+
+func init() {
+	cli.AppHelpTemplate += "\nContribute to this tool here: https://github.com/dops-cli <3\n"
+}
+
 func main() {
 
+	for _, module := range modules.RegisteredModules {
+		CliFlags = append(CliFlags, module.GetFlags()...)
+		CliCommands = append(CliCommands, module.GetCommands()...)
+	}
+
+	app := &cli.App{
+		Name:    "dops",
+		Version: "v0.0.1",
+		Authors: []*cli.Author{
+			{
+				Name:  "Marvin Wendt",
+				Email: "dops@marvinjwendt.com",
+			},
+		},
+		Copyright: "(c) 2020 Marvin Wendt",
+		HelpName:  "dops",
+		Usage:     "DevOps",
+		Flags:     CliFlags,
+		Commands:  CliCommands,
+	}
+
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
