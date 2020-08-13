1d414edd5fec1b37e36e2184bf04876a06dae8bd
MarvinJWendt <git@marvinjwendt.com>
2020-08-13 19:12:13 +0200

    refactor: change module registration

--- module/module.go
+++ module/module.go
@@ -11,12 +11,12 @@ import (
 
 // ActiveGlobalFlags contains all global flags.
 // If a global flag is not in this slice, it won't be activated.
-var ActiveGlobalFlags = []GlobalFlag{debug.Flag{}, raw.Flag{}}
+var ActiveGlobalFlags []GlobalFlag
 
 // ActiveModules contains all available modules.
 // If a module is not in this slice, it won't be activated.
 // Except for the module `modules`, which is registered in the main package.
-var ActiveModules = []Module{bulkdownload.Module{}, update.Module{}, extract.Module{}}
+var ActiveModules []Module
 
 // Module is the interface of each module available in dops.
 // Each module must return at least one command.
@@ -28,4 +28,24 @@ type Module interface {
 // Each flag module must return at least one flag.
 type GlobalFlag interface {
 	GetFlags() []cli.Flag
+}
+
+// * <<< Add modules and global flags here! >>> *
+func init() {
+	// Add the global flags
+	addGlobalFlag(debug.Flag{})
+	addGlobalFlag(raw.Flag{})
+
+	// Add modules
+	addModule(bulkdownload.Module{})
+	addModule(extract.Module{})
+	addModule(update.Module{})
+}
+
+func addModule(module Module) {
+	ActiveModules = append(ActiveModules, module)
+}
+
+func addGlobalFlag(flag GlobalFlag) {
+	ActiveGlobalFlags = append(ActiveGlobalFlags, flag)
 }
