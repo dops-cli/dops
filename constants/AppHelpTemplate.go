069d15b9530a5ea3071e0b54b624075f6d44a538
MarvinJWendt <git@marvinjwendt.com>
2020-08-13 04:59:49 +0200

    feat: add more color to dops help

--- constants/AppHelpTemplate.go
+++ constants/AppHelpTemplate.go
@@ -8,13 +8,13 @@ import (
 // AppHelpTemplate contains the template of dops help text.
 var AppHelpTemplate = fmt.Sprintf(color.HiCyanString("\nDops - CLI DevOps Toolkit") + `
 
-{{if .VisibleFlags}}` + color.HiYellowString(`Global options`) + `
-   {{range $index, $option := .VisibleFlags}}{{if $index}}
-   {{end}}{{$option}}{{end}}{{end}}
+{{if .VisibleFlags}}` + color.New(color.FgHiYellow, color.Underline).Sprint(`Global options`) + `
+  ` + color.YellowString(`{{range $index, $option := .VisibleFlags}}{{if $index}}`) + `
+  ` + color.YellowString(`{{end}}{{$option}}{{end}}{{end}}`) + `
 
-{{if .VisibleCommands}}` + color.HiYellowString(`Modules`) + `{{range .VisibleCategories}}{{if .Name}}
-[` + color.HiCyanString(`{{.Name}}`) + `]{{range .VisibleCommands}}
-  · ` + color.HiMagentaString(`{{join .Names ", "}}`) + color.HiRedString(`{{"\t|\t"}}`) + `{{.Usage}}{{end}}{{else}}{{range .VisibleCommands}}
-  · ` + color.HiMagentaString(`{{join .Names ", "}}`) + color.HiRedString(`{{"\t|\t"}}`) + `{{.Usage}}{{end}}{{end}}{{end}}{{end}}
+{{if .VisibleCommands}}` + color.New(color.FgHiYellow, color.Underline).Sprint(`Modules`) + `{{range .VisibleCategories}}{{if .Name}}
+  [` + color.HiCyanString(`{{.Name}}`) + `]{{range .VisibleCommands}}
+    · ` + color.HiMagentaString(`{{join .Names ", "}}`) + color.HiRedString(`{{"\t|\t"}}`) + `{{.Usage}}{{end}}{{else}}{{range .VisibleCommands}}
+    · ` + color.HiMagentaString(`{{join .Names ", "}}`) + color.HiRedString(`{{"\t|\t"}}`) + `{{.Usage}}{{end}}{{end}}{{end}}{{end}}
 
-` + color.HiRedString("Contribute to this tool here: https://github.com/dops-cli ") + color.RedString("<3"))
+` + color.HiRedString("Contribute to this tool here: https://github.com/dops-cli ") + color.RedString("<3\n"))
