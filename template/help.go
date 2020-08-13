1939da0c6afb2fbf7d919774fdfcfa3a1ede8aff
MarvinJWendt <git@marvinjwendt.com>
2020-08-13 11:08:23 +0200

    feat: change color theme

--- template/help.go
+++ template/help.go
@@ -4,34 +4,32 @@ import (
 	. "github.com/dops-cli/dops/say/color"
 )
 
-var SectionColor = New(FgHiYellow, Underline)
-
 // CommandHelp contains the template of dops modules help text.
-var CommandHelp = HiCyanString("\n{{.HelpName}}") + ` - ` + HiMagentaString("{{.Usage}}") + `
+var CommandHelp = Primary("\n{{.HelpName}}") + ` - ` + Secondary("{{.Usage}}") + `
 
-` + HiCyanString("Usage:") + ` {{if .UsageText}}{{.UsageText}}{{else}}{{.HelpName}}{{if .VisibleFlags}} [options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}{{end}}
-{{if .Aliases}}` + HiCyanString("Aliases:") + `  {{join .Aliases ", "}}{{end}}
-{{if .Category}}` + HiCyanString("Category:") + ` {{.Category}}{{end}}{{if .Description}}
+` + Primary("Usage:") + ` {{if .UsageText}}{{.UsageText}}{{else}}{{.HelpName}}{{if .VisibleFlags}} [options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}{{end}}
+{{if .Aliases}}` + Primary("Aliases:") + `  {{join .Aliases ", "}}{{end}}
+{{if .Category}}` + Primary("Category:") + ` {{.Category}}{{end}}{{if .Description}}
 
-` + SectionColor.Sprint("Description") + `
+` + Section("Description") + `
 {{.Description}}{{end}}{{if .VisibleFlags}}
 
-` + SectionColor.Sprint("Options") + `
-   {{range .VisibleFlags}}` + YellowString("{{.}}") + `
+` + Section("Options") + `
+   {{range .VisibleFlags}}` + Flag("{{.}}") + `
    {{end}}{{end}}
 `
 
 // AppHelp contains the template of dops help text.
-var AppHelp = HiCyanString("\nDOPS - CLI DevOps Toolkit") + `
+var AppHelp = Primary("\nDOPS - CLI DevOps Toolkit") + `
 
-{{if .VisibleFlags}}` + New(FgHiYellow, Underline).Sprint(`Global options`) + `
-  ` + YellowString(`{{range $index, $option := .VisibleFlags}}{{if $index}}`) + `
-  ` + YellowString(`{{end}}{{$option}}{{end}}{{end}}`) + `
+{{if .VisibleFlags}}` + Section(`Global options`) + `
+  ` + Flag(`{{range $index, $option := .VisibleFlags}}{{if $index}}`) + `
+  ` + Flag(`{{end}}{{$option}}{{end}}{{end}}`) + `
 
-{{if .VisibleCommands}}` + New(FgHiYellow, Underline).Sprint(`Modules`) + `{{range .VisibleCategories}}{{if .Name}}
-  [` + HiCyanString(`{{.Name}}`) + `]{{range .VisibleCommands}}
-    · ` + HiMagentaString(`{{join .Names ", "}}`) + HiRedString(`{{"\t|\t"}}`) + `{{.Usage}}{{end}}{{else}}{{range .VisibleCommands}}
-    · ` + HiMagentaString(`{{join .Names ", "}}`) + HiRedString(`{{"\t|\t"}}`) + `{{.Usage}}{{end}}{{end}}{{end}}{{end}}
+{{if .VisibleCommands}}` + Section(`Modules`) + `{{range .VisibleCategories}}{{if .Name}}
+  [` + Primary(`{{.Name}}`) + `]{{range .VisibleCommands}}
+    · ` + Secondary(`{{join .Names ", "}}`) + Separator(`{{"\t|\t"}}`) + `{{.Usage}}{{end}}{{else}}{{range .VisibleCommands}}
+    · ` + Secondary(`{{join .Names ", "}}`) + Separator(`{{"\t|\t"}}`) + `{{.Usage}}{{end}}{{end}}{{end}}{{end}}
 
 ` + HiRedString("Contribute to this tool here: https://github.com/dops-cli ") + RedString("<3\n")
 
