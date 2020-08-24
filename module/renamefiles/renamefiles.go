df1d2af51dbc2a520923086082f562b64e4f3d7b
MarvinJWendt <git@marvinjwendt.com>
2020-08-24 22:46:54 +0200

    feat: add module `renamefiles`

--- module/renamefiles/renamefiles.go
+++ module/renamefiles/renamefiles.go
@@ -1,7 +1,16 @@
 package renamefiles
 
 import (
+	"crypto/md5"
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
 
 	"github.com/dops-cli/dops/cli"
 
@@ -11,7 +20,7 @@ import (
 // Module returns the created module
 type Module struct{}
 
-// GetCommands returns the commands of the module
+// GetModuleCommands returns the commands of the module
 func (Module) GetModuleCommands() []*cli.Command {
 	return []*cli.Command{
 		{
@@ -22,19 +31,54 @@ func (Module) GetModuleCommands() []*cli.Command {
 The pattern could be a timestamp, or the hashcode of the file, among others.`,
 			Category: categories.IO,
 			Action: func(context *cli.Context) error {
-				fmt.Println("MODE:", context.Option("mode"))
-				fmt.Println("TEST:", context.String("test"))
+				option := context.Option("pattern")
+
+				files, err := ioutil.ReadDir(".")
+				if err != nil {
+					fmt.Println(err.Error())
+				}
+
+				hasher := sha1.New()
+
+				for _, file := range files {
+					if file.IsDir() {
+						continue
+					}
+
+					switch option {
+					case "sha-1":
+						hasher = sha1.New()
+						break
+					case "md5":
+						hasher = md5.New()
+						break
+					}
+
+					content, _ := os.Open(file.Name())
+
+					if _, err := io.Copy(hasher, content); err != nil {
+						log.Fatal(err)
+					}
+
+					_ = content.Close()
+
+					fmt.Printf("%s -> %s%s\n", file.Name(), hex.EncodeToString(hasher.Sum(nil)), filepath.Ext(file.Name()))
+					err := os.Rename(file.Name(), hex.EncodeToString(hasher.Sum(nil))+path.Ext(file.Name()))
+
+					if err != nil {
+						fmt.Println(err.Error())
+					}
+
+				}
+
 				return nil
 			},
 			Flags: []cli.Flag{
 				&cli.OptionFlag{
-					Name:    "mode",
-					Aliases: []string{"m"},
-					Usage:   "",
-					Options: []string{"sha-128", "md5"},
-				},
-				&cli.StringFlag{
-					Name: "test",
+					Name:    "pattern",
+					Aliases: []string{"p"},
+					Usage:   "Rename all files with `OPTION`",
+					Options: []string{"sha-1", "md5"},
 				},
 			},
 		},
