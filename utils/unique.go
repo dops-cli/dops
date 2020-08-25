5d224912f89d0973eed1ae48d963e5adfe1c504b
MarvinJWendt <git@marvinjwendt.com>
2020-08-25 04:04:48 +0200

    refactor: add unique methods for slices

---
+++ utils/unique.go
@@ -0,0 +1,27 @@
+package utils
+
+// UniqueIntSlice removes duplicates from a slice
+func UniqueIntSlice(intSlice []int) []int {
+	keys := make(map[int]bool)
+	var list []int
+	for _, entry := range intSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
+
+// UniqueStringSlice removes duplicates from a slice
+func UniqueStringSlice(stringSlice []string) []string {
+	keys := make(map[string]bool)
+	var list []string
+	for _, entry := range stringSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
