6efa578ffe881a0509dc2a381985bc09d3cbdde1
MarvinJWendt <git@marvinjwendt.com>
2020-09-06 19:27:54 +0200

    refactor: bump version to "v1.22.1"

--- dops.go
+++ dops.go
@@ -39,7 +39,7 @@ func main() {
 		Name:                 "dops",
 		HelpName:             "dops",
 		Usage:                "CLI DevOps Toolkit",
-		Version:              "v1.22.0", // <---VERSION---> This comment is used for CI, do NOT modify it!
+		Version:              "v1.22.1", // <---VERSION---> This comment is used for CI, do NOT modify it!
 		Commands:             global.CliCommands,
 		Flags:                global.CliFlags,
 		EnableBashCompletion: true,
