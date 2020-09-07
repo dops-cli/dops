0329d87cddbfb0f67cb280f78046e67423ef67de
MarvinJWendt <git@marvinjwendt.com>
2020-09-07 21:03:14 +0200

    feat: add update command to `update` module

--- module/update/update.go
+++ module/update/update.go
@@ -26,11 +26,11 @@ func (Module) GetModuleCommands() []*cli.Command {
 				if runtime.GOOS == "windows" {
 					cli.ShowVersion(c)
 					say.Text(color.Primary("To update dops, open a new powershell with admin privileges and run:"))
-					say.Text(color.New(color.BgBlack).Sprint("iwr -useb dops-cli.com/get/windows | iex"))
+					say.Text(color.SHiRed("iwr -useb dops-cli.com/get/windows | iex"))
 				} else {
 					cli.ShowVersion(c)
-					say.Warning("Automatic updates are not supported for " + runtime.GOOS + ", yet.")
-					say.Text("Please visit https://github.com/dops-cli/dops/releases to download the latest version.")
+					say.Text("To update dops, open a terminal and run")
+					say.Text(color.SHiRed("curl -s https://dops-cli.com/get/linux | sudo bash"))
 				}
 
 				return nil
