c4cbf01c64a021f10e43737bc4b71b77a7476975
MarvinJWendt <git@marvinjwendt.com>
2020-09-10 23:59:40 +0200

    refactor: change module structure of `extract text`

--- module/extract/extract.go
+++ module/extract/extract.go
@@ -1,12 +1,8 @@
 package extract
 
 import (
-	"regexp"
-
-	"github.com/dops-cli/dops/cli"
-	"github.com/dops-cli/dops/utils"
-
 	"github.com/dops-cli/dops/categories"
+	"github.com/dops-cli/dops/cli"
 )
 
 // Module returns the created module
@@ -21,54 +17,7 @@ func (Module) GetModuleCommands() []*cli.Command {
 			Description: `Extract contains multiple data extractors, which can be used to extract data from a file, URL or stdin.`,
 			Category:    categories.DataAnalysis,
 			Subcommands: []*cli.Command{
-				{
-					Name:        "text",
-					Aliases:     []string{"t", "string", "strings", "s"},
-					Usage:       "Extracts text from data",
-					Examples:    nil,
-					Description: "This can be used to extract text using a predefined or a custom regex.",
-					Action: func(c *cli.Context) error {
-						regex := c.String("regex")
-						input := c.Path("input")
-						output := c.String("output")
-						appendFlag := c.Bool("appendFlag")
-
-						var foundStrings []string
-
-						r, err := regexp.Compile(regex)
-						if err != nil {
-							return err
-						}
-
-						foundStrings = r.FindAllString(utils.Input(input), -1)
-						utils.Output(output, foundStrings, appendFlag)
-
-						return nil
-					},
-					Flags: []cli.Flag{
-						&cli.StringFlag{
-							Name:    "regex",
-							Aliases: []string{"r"},
-							Usage:   "extracts matching strings with `PATTERN`",
-						},
-						&cli.PathFlag{
-							Name:      "input",
-							Aliases:   []string{"i"},
-							Usage:     "use `FILE` as input",
-							TakesFile: true,
-						},
-						&cli.StringFlag{
-							Name:    "output",
-							Aliases: []string{"o"},
-							Usage:   "outputs to directory `DIR`",
-						},
-						&cli.BoolFlag{
-							Name:    "append",
-							Aliases: []string{"a"},
-							Usage:   "append instead of overriding output",
-						},
-					},
-				},
+				Text(),
 			},
 		},
 	}
---
+++ module/extract/text.go
@@ -0,0 +1,60 @@
+package extract
+
+import (
+	"regexp"
+
+	"github.com/dops-cli/dops/cli"
+	"github.com/dops-cli/dops/utils"
+)
+
+// Text module
+func Text() *cli.Command {
+	return &cli.Command{
+		Name:        "text",
+		Aliases:     []string{"t", "string", "strings", "s"},
+		Usage:       "Extracts text from data",
+		Examples:    nil,
+		Description: "This can be used to extract text using a predefined or a custom regex.",
+		Action: func(c *cli.Context) error {
+			regex := c.String("regex")
+			input := c.Path("input")
+			output := c.String("output")
+			appendFlag := c.Bool("appendFlag")
+
+			var foundStrings []string
+
+			r, err := regexp.Compile(regex)
+			if err != nil {
+				return err
+			}
+
+			foundStrings = r.FindAllString(utils.Input(input), -1)
+			utils.Output(output, foundStrings, appendFlag)
+
+			return nil
+		},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "regex",
+				Aliases: []string{"r"},
+				Usage:   "extracts matching strings with `PATTERN`",
+			},
+			&cli.PathFlag{
+				Name:      "input",
+				Aliases:   []string{"i"},
+				Usage:     "use `FILE` as input",
+				TakesFile: true,
+			},
+			&cli.StringFlag{
+				Name:    "output",
+				Aliases: []string{"o"},
+				Usage:   "outputs to directory `DIR`",
+			},
+			&cli.BoolFlag{
+				Name:    "append",
+				Aliases: []string{"a"},
+				Usage:   "append instead of overriding output",
+			},
+		},
+	}
+}
